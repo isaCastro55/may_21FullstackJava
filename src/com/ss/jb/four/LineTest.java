671492cc3150178bfd9e142a703bf93b4c2f1dbb
Bellac12345 <isabebellacastro@ufl.edu>
2021-05-14 11:04:32 -0400

    Day4Assignments

---
+++ src/com/ss/jb/four/LineTest.java
@@ -0,0 +1,35 @@
+package com.ss.jb.four;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class LineTest {
+
+    private Line lin = new Line(4,6,10,30);
+
+    @Test
+    public void slopeTest(){
+
+        assertEquals(new Double (4.0),lin.getSlope(), .0001);
+        assertNotEquals(new Double(6.0), lin.getSlope(), .0001);
+    }
+
+    @Test
+    public void distanceTest(){
+
+        assertEquals(new Double (24.73863),lin.getDistance(), .0001);
+        assertNotEquals(new Double(3.5), lin.getDistance(), .0001);
+    }
+
+    @Test
+    public void parallelTest(){
+        Line test = new Line(1, 9, 2, 13);
+        Line fail = new Line(7,30,10,20);
+        assertTrue(lin.parallelTo(test));
+        assertFalse(lin.parallelTo(fail));
+        assertNotEquals(true, lin.parallelTo(fail));
+    }
+
+
+}
